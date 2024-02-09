6355152f7dbbb0ad422ffd30496852a9b2449d5f
vaibhavugale <ugalevaibhav00@gmail.com>
2024-02-09 13:13:31 +0530

    ok

--- backend/controllers/Auth.js
+++ backend/controllers/Auth.js
@@ -35,16 +35,10 @@ exports.signIn = async (req, res) => {
         expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
         httpOnly: true,
         secure: true,
-<<<<<<< HEAD
         path:"/",
         sameSite:'None',
         overwrite:true
-=======
-        overwrite:true,
-        sameSite: 'strict',
-        domain:".onrender.com"
-  
->>>>>>> cb41946 (auth logic updated)
+
       };
       await res.cookie("token", token, options)
       return  res.status(200).json({
